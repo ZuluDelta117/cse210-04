2a3f5aad5cbf3ada90dece3e447d0271c84a8a79
ZuluDelta117 <97050214+ZuluDelta117@users.noreply.github.com>
2022-10-27 13:48:43 -0600

    Update artifact.py

--- greed/game/casting/artifact.py
+++ greed/game/casting/artifact.py
@@ -1,38 +1,36 @@
 from game.casting.actor import Actor
+import random
 
-# coppied from the rfk assignment earlier this week
-# No chnages needed (Zack D.)
-
-# I do not think this classes is needed, but due to other files calling the parent funciton through the child function it cannot be removed. 
-# We also are required to have 8 classes for this assignment and this class helps us complete that requirment. (Zack D.)
-
-# I think we can add something to the code that will use this class to congragulate the player when they get a gem and to tell them they suck if they get a rock (Zack D.)
 
 class Artifact(Actor):
     """
     An item of cultural or historical interest. 
+    It may be a gem (good) or a rock (bad).
     
-    The responsibility of an Artifact is to provide a message about itself.
+    The artifact provides a point if it is a gem, 
+    or takes away a point if it is a rock.
 
     Attributes:
-        _message (string): A short description about the artifact.
+        Inherits all attributes of actor.
+        _type (string): It is either a gem or a rock.
+        _value (int): +1 for a gem, -1 for a rock.
     """
     def __init__(self):
         super().__init__()
-        self._message = ""
-        
-    def get_message(self):
-        """Gets the artifact's message.
-        
-        Returns:
-            string: The message.
-        """
-        return self._message
+        self._value = 1
     
-    def set_message(self, message):
-        """Updates the message to the given one.
+    def get_value(self):
+        """Gets the artifact's value.
+            
+            Returns:
+                int: The value(+1 or -1).
+        """ 
+        return self._value
+
+    def set_value(self, new_value):
+        """Updates the value to the given one.
         
         Args:
-            message (string): The given message.
+            new_value (int): The given value.
         """
-        self._message = message
+        self._value = new_value
